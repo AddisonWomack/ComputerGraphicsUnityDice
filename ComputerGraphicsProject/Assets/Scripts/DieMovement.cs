83160963493e72b5bdb54eb603f88a78b03b09f1
Addison <addison.womack@ou.edu>
2019-02-09 18:03:11 -0600

    Add moveable die and environment border

---
+++ ComputerGraphicsProject/Assets/Scripts/DieMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieMovement : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        speed = 50;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizonal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        var v1 = new Vector3(moveHorizonal, 0.0f, moveVertical);
+
+        rigidBody.AddForce(v1 * speed);
+    }
+}

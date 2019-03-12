60393a3c6c1cb5dcb276963c8f1a965ba5232a97
Addison <addison.womack@ou.edu>
2019-03-12 14:34:03 -0500

    Makes the die visible (and initally unaffected by gravity) prior to launch. The die can be rotated by holding the arrow keys, which rotate the die faster as the space bar is held down. While the space bar is held down a pitch is also applied to the die, which makes it appear faster and faster prior to the release of the space bar. Once the space bar is released a 0.8 second respawn timer is triggered, which spawns a new object for the user to throw. Also gravity is enabled once an object is thrown.

--- ComputerGraphicsProject/Assets/Scripts/PlayerController.cs
+++ ComputerGraphicsProject/Assets/Scripts/PlayerController.cs
@@ -17,16 +17,27 @@ public class PlayerController : MonoBehaviour
     public int objectCreationPositionY;
     public int objectCreationPositionZ;
 
+    // most recent tally of thrown objects
     private int currentDieResult;
 
+    // force applied to object causing it to move forward
     private float objectReleaseIntensity;
 
+    private float angularVelocityPitchRate;
+    private float angularVelocityRollRate;
+    private float angularVelocityYawRate;
 
     // type of object being created
     private togglableObject objectSelection;
 
+    // object to be thrown
+    private Rigidbody CurrentObjectToBeThrown;
+
+    private bool isThrown = false;
+
     private const float defaultObjectReleaseIntensity = 30;
-    private const float intensityReleaseChangePerFrame = 5;
+    private const float intensityReleaseChangePerFrame = 10;
+    private const float objectRespawnTime = 0.8f;
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +45,7 @@ public class PlayerController : MonoBehaviour
         // set to die by default
         objectSelection = togglableObject.Die;
         objectReleaseIntensity = defaultObjectReleaseIntensity;
+        CurrentObjectToBeThrown = instantiateSelectedObject();
     }
 
     // Update is called once per frame
@@ -42,17 +54,14 @@ public class PlayerController : MonoBehaviour
         // user is throwing an object
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            Rigidbody objectToBeCreated = instantiateSelectedObject();
-
-            objectToBeCreated.AddForce(new Vector3(0.0f, 0.0f, objectReleaseIntensity));
-
-            objectReleaseIntensity = defaultObjectReleaseIntensity;
+            throwObject();
         }
 
         // user is charging up the dice throw
         if (Input.GetKey(KeyCode.Space))
         {
             objectReleaseIntensity += intensityReleaseChangePerFrame;
+            rotateCurrentObjectZ(objectReleaseIntensity);
         }
 
         // clear playing area of rollable objects
@@ -61,6 +70,68 @@ public class PlayerController : MonoBehaviour
             deleteRollableObjects();
         }
 
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rotateCurrentObjectX(-1 * objectReleaseIntensity);
+        } else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rotateCurrentObjectX(objectReleaseIntensity);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotateCurrentObjectY(objectReleaseIntensity);
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotateCurrentObjectY(-1 * objectReleaseIntensity);
+        }
+
+    }
+
+    private void rotateCurrentObjectX(float delta)
+    {
+        if (!isThrown)
+        {
+            var deltaQuarternion = Quaternion.Euler(delta * Time.deltaTime, 0, 0);
+            CurrentObjectToBeThrown.MoveRotation(CurrentObjectToBeThrown.rotation * deltaQuarternion);
+        }
+    }
+
+    private void rotateCurrentObjectY(float delta)
+    {
+        if (!isThrown)
+        {
+            var deltaQuarternion = Quaternion.Euler(0, delta * Time.deltaTime, 0);
+            CurrentObjectToBeThrown.MoveRotation(CurrentObjectToBeThrown.rotation * deltaQuarternion);
+        }
+    }
+
+    private void rotateCurrentObjectZ(float delta)
+    {
+        if (!isThrown)
+        {
+            var deltaQuarternion = Quaternion.Euler(0, 0, delta * Time.deltaTime);
+            CurrentObjectToBeThrown.MoveRotation(CurrentObjectToBeThrown.rotation * deltaQuarternion);
+        }
+    }
+
+    private void respawnObject()
+    {
+        CurrentObjectToBeThrown = instantiateSelectedObject();
+        isThrown = false;
+    }
+
+    private void throwObject()
+    {
+        // enable gravity
+        CurrentObjectToBeThrown.useGravity = true;
+        // launch the object forward with the given intensity
+        CurrentObjectToBeThrown.AddForce(new Vector3(0.0f, 0.0f, objectReleaseIntensity));
+        // reset object release intensity to default
+        objectReleaseIntensity = defaultObjectReleaseIntensity;
+        isThrown = true;
+        // calls method after specified amount of time
+        Invoke("respawnObject", objectRespawnTime);
     }
 
     // deletes all present rollable objects
@@ -89,17 +160,26 @@ public class PlayerController : MonoBehaviour
     // constructs a RigidBody based on the user's current selection
     private Rigidbody instantiateSelectedObject()
     {
+        Rigidbody rigidbody;
+
+        var creationPosition = new Vector3(objectCreationPositionX, objectCreationPositionY, objectCreationPositionZ);
+
         // creates selected object with default orientation at given x,y,z
         switch (objectSelection)
         {
             case togglableObject.Die:
-                return Instantiate(dieObject,
-                    new Vector3(objectCreationPositionX, objectCreationPositionY, objectCreationPositionZ),
+                rigidbody = Instantiate(dieObject,
+                    creationPosition,
                     Quaternion.identity);
+                break;
             default:
-                return Instantiate(dieObject,
-                    new Vector3(objectCreationPositionX, objectCreationPositionY, objectCreationPositionZ),
+                rigidbody = Instantiate(dieObject,
+                    creationPosition,
                     Quaternion.identity);
+                break;
         }
+        // initially not affected by gravity
+        rigidbody.useGravity = false;
+        return rigidbody;
     }
 }
